b78db78ad7c95b3e9f7591ec9f0908015a686a09
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2022-08-11 14:44:03 -0300

    select musics

--- controllers/spotify.js
+++ controllers/spotify.js
@@ -18,7 +18,7 @@ exports.getGenres = async (req, res, next) => {
   const data = {
     grant_type: "client_credentials",
   };
-  
+
   try {
     const response = await axios.post(
       "https://accounts.spotify.com/api/token",
@@ -26,26 +26,29 @@ exports.getGenres = async (req, res, next) => {
       headers
     );
 
-    const genresResponse = await axios.get('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + response.data.access_token
+    const genresResponse = await axios.get(
+      "https://api.spotify.com/v1/recommendations/available-genre-seeds",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + response.data.access_token,
+        },
       }
-    })
-    const allGenres = genresResponse.data.genres 
-    const genres = getRandomGenres(allGenres)
-    res.send(genres)
+    );
+    const allGenres = genresResponse.data.genres;
+    const genres = getRandomGenres(allGenres);
+    res.send(genres);
   } catch (error) {
     console.log(error);
-    res.send(error)
+    res.send(error);
   }
-}
+};
 
 exports.getSongs = async (req, res, next) => {
   const headers = {
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/x-www-form-urlencoded',
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded",
     },
     auth: {
       username: client_id,
@@ -53,35 +56,46 @@ exports.getSongs = async (req, res, next) => {
     },
   };
   const data = {
-    grant_type: 'client_credentials',
+    grant_type: "client_credentials",
   };
 
   try {
     const response = await axios.post(
-      'https://accounts.spotify.com/api/token',
+      "https://accounts.spotify.com/api/token",
       qs.stringify(data),
       headers
     );
-    const genre = req.query.genre
-    console.log(req)
-    const songsResponse = await axios.get(`https://api.spotify.com/v1/search?q=genre:${genre}&type=track`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + response.data.access_token
+    const genre = req.query.genre;
+    const songsResponse = await axios.get(
+      `https://api.spotify.com/v1/search?q=genre:${genre}&type=track&limit=4`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + response.data.access_token,
+        },
       }
-    })
-    const songs = songsResponse.data
-
-    res.send(songs)
+    );
+    const songs = songsResponse.data;
+    const parsedResponse = []
+    for(let i =0; i < 4 ; i++){
+      parsedResponse.push({
+      song: songs.tracks.items[i].name,
+      album: songs.tracks.items[i].album.name,
+      audio: songs.tracks.items[i].preview_url,
+      picture: songs.tracks.items[i].album.images[0],
+      artist: songs.tracks.items[i].artists[0].name     
+      })
+    }
+    res.send(parsedResponse);
   } catch (error) {
     console.log(error);
-    res.send(error)
+    res.send(error);
   }
-}
+};
 
 function getRandomGenres(array) {
-  const indexOne = Math.floor(Math.random() * array.length)
-  array.splice(indexOne,1)
-  const indexTwo = Math.floor(Math.random() * array.length)
-  return [array[indexOne],array[indexTwo]]
+  const indexOne = Math.floor(Math.random() * array.length);
+  array.splice(indexOne, 1);
+  const indexTwo = Math.floor(Math.random() * array.length);
+  return [array[indexOne], array[indexTwo]];
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,4 @@ var spotifyController = require('../controllers/spotify.js');
 /* GET home page. */
 router.get('/genres', spotifyController.getGenres);
 router.get('/songs', spotifyController.getSongs);
-
 module.exports = router;
